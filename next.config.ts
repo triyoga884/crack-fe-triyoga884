0440ce9c9874f142fc004ee4ba1ddc62ae93af46
triyoga884 <triyoga884@gmail.com>
2025-11-19 17:03:50 +0700

    add img and some ui library from shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [new URL('https://placehold.co/**')],
+//   },
+// };
+
+// export default nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [new URL('https://placehold.co/**')],
+  },
 };
-
-export default nextConfig;
