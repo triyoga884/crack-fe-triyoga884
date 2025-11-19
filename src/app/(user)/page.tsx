54ade0154120549c1a22ed95b2a74d06c2826bb8
triyoga884 <triyoga884@gmail.com>
2025-11-19 11:10:56 +0700

    nextjs + shadcn setup

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="bg-primary h-100">
+      <h1 className="bg-accent">Hello</h1>
+    </div>
+  );
+}
