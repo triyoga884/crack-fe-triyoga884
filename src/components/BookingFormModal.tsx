48935b5f829a4dbd0a211ed5c2946653ed73fa70
triyoga884 <triyoga884@gmail.com>
2025-12-04 15:23:18 +0700

    feat: implement booking form modal with validation and submission logic

---
+++ src/components/BookingFormModal.tsx
@@ -0,0 +1,249 @@
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field';
+import { ChevronDownIcon } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { useRouter } from 'next/navigation';
+
+const bookingSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.email('Invalid email address'),
+  contact: z.string().min(10, 'Contact number is required'),
+  companyName: z.string().optional(),
+  startDate: z.date(),
+  endDate: z.date(),
+});
+
+function BookingFormModal({ data }: any) {
+  console.log(data);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof bookingSchema>>({
+    resolver: zodResolver(bookingSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      contact: '',
+      companyName: '',
+      startDate: undefined,
+      endDate: undefined,
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof bookingSchema>) => {
+    console.log(data);
+    router.push('/checkout');
+  };
+
+  const [calenderStartOpen, setCalendarStartOpen] = useState(false);
+  const [calenderEndOpen, setCalendarEndOpen] = useState(false);
+
+  return (
+    <Dialog>
+      <form id="booking-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <DialogTrigger asChild>
+          <Button>Book Now</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>Booking Form</DialogTitle>
+            <DialogDescription>
+              Please fill form below. Click ok when you&apos;re done.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup className="grid md:grid-cols-2 gap-4">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="name">Name</FieldLabel>
+                  <Input
+                    id="name"
+                    {...field}
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    {...field}
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="contact"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="contact">Contact</FieldLabel>
+                  <Input
+                    id="contact"
+                    type="contact"
+                    {...field}
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="companyName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="companyName">Company Name</FieldLabel>
+                  <Input
+                    id="companyName"
+                    type="companyName"
+                    {...field}
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="startDate"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <Label htmlFor="date">Start Date</Label>
+                  <Popover
+                    open={calenderStartOpen}
+                    onOpenChange={setCalendarStartOpen}
+                  >
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        id="date"
+                        className="justify-between"
+                      >
+                        {field.value
+                          ? new Date(field.value).toLocaleDateString()
+                          : 'Select date'}
+                        <ChevronDownIcon />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <Calendar
+                        mode="single"
+                        selected={
+                          field.value ? new Date(field.value) : undefined
+                        }
+                        captionLayout="dropdown"
+                        onSelect={(date) => {
+                          field.onChange(date);
+                          setCalendarStartOpen(false);
+                        }}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="endDate"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <Label htmlFor="date">End Date</Label>
+                  <Popover
+                    open={calenderEndOpen}
+                    onOpenChange={setCalendarEndOpen}
+                  >
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        id="date"
+                        className="justify-between"
+                      >
+                        {field.value
+                          ? new Date(field.value).toLocaleDateString()
+                          : 'Select date'}
+                        <ChevronDownIcon />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <Calendar
+                        mode="single"
+                        selected={
+                          field.value ? new Date(field.value) : undefined
+                        }
+                        captionLayout="dropdown"
+                        onSelect={(date) => {
+                          field.onChange(date);
+                          setCalendarEndOpen(false);
+                        }}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit" form="booking-form">
+              Save changes
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
+
+export default BookingFormModal;
