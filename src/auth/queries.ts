0915d78931687b26ce71307abb8d1f8685866e27
triyoga884 <triyoga884@gmail.com>
2026-01-14 00:01:09 +0700

    fix delete unused function

--- src/auth/queries.ts
+++ src/auth/queries.ts
@@ -1,42 +1,14 @@
-// src/features/auth/queries.ts
 import { useQuery } from '@tanstack/react-query';
 import { getMe } from './api';
-import { setAccessToken } from '../lib/fetcher';
 
 export function useMe() {
-  // const { isSuccess } = useAuthBootstrap();
-
   return useQuery({
     queryKey: ['me'],
     queryFn: getMe,
-    retry: false, // auth errors are real
-    staleTime: Infinity, // user rarely changes
+    retry: false,
+    staleTime: Infinity,
     gcTime: Infinity,
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
-    // enabled: isSuccess,
   });
 }
-
-// export function useAuthBootstrap() {
-//   return useQuery({
-//     queryKey: ['auth', 'bootstrap'],
-//     queryFn: async () => {
-//       const res = await fetch(
-//         `${process.env.NEXT_PUBLIC_LOCAL_API}/auth/refresh`,
-//         {
-//           method: 'POST',
-//           credentials: 'include',
-//         }
-//       );
-
-//       if (!res.ok) throw new Error('No session');
-
-//       const data = await res.json();
-//       setAccessToken(data.accessToken);
-//       return true;
-//     },
-//     retry: false,
-//     staleTime: Infinity,
-//   });
-// }
