dcf52e62271ccb5f18b8a59e23f3310efd61ea3e
Tri Yoga Febriansyah <triyoga884@gmail.com>
2026-01-14 01:03:25 +0700

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,6 @@
 import LoginForm from '@/components/LoginForm';
-import { useAuth } from '@/hooks/useAuth';
-import { redirect } from 'next/navigation';
 
 function page() {
-    const { isLoading, isAuthenticated } = useAuth();
-  
-  if (isLoading) return <div>Loading...</div>;
-
-  if (!isAuthenticated) redirect('/');
-  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-600">
       <LoginForm />
