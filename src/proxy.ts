7aa43e4b7fa9f93c9bb5f9511758a559222730f9
Tri Yoga Febriansyah <triyoga884@gmail.com>
2026-01-14 00:15:19 +0700

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,16 +7,16 @@ const PROTECTED_PAGES = ['/dashboard', '/checkout', '/profile'];
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  const hasRefreshToken = req.cookies.has('refresh_token');
-  // const access_token = req.cookies.get('access_token')?.value
+  // const hasRefreshToken = req.cookies.has('refresh_token');
+  const access_token = req.cookies.get('access_token')?.value;
 
   // 🔒 Logged-in users should NOT access login/register
-  if (hasRefreshToken && AUTH_PAGES.includes(pathname)) {
+  if (access_token && AUTH_PAGES.includes(pathname)) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
   // 🔑 Logged-out users should NOT access protected pages
-  if (!hasRefreshToken && PROTECTED_PAGES.some((p) => pathname.startsWith(p))) {
+  if (!access_token && PROTECTED_PAGES.some((p) => pathname.startsWith(p))) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
